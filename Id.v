40c6f789b7b3efc76b7cc00d58fee2b0a8d1a83c
rozplokhas <rozplokhas@gmail.com>
2017-03-24 02:43:37 +0300

    Add Id and State proofs

--- Id.v
+++ Id.v
@@ -12,53 +12,118 @@ Reserved Notation "m <<  n" (at level 70, no associativity).
 
 Inductive le_id : id -> id -> Prop :=
   le_conv : forall n m, n <= m -> (Id n) <<= (Id m)
-where "n <<= m" := (le_id n m).   
+where "n <<= m" := (le_id n m).
 
 Inductive lt_id : id -> id -> Prop :=
   lt_conv : forall n m, n < m -> (Id n) << (Id m)
-where "n << m" := (lt_id n m).   
+where "n << m" := (lt_id n m).
 
 Inductive gt_id : id -> id -> Prop :=
   gt_conv : forall n m, n > m -> (Id n) >> (Id m)
-where "n >> m" := (gt_id n m).   
+where "n >> m" := (gt_id n m).
 
 Notation "n <<= m" := (le_id n m).
 Notation "n >>  m" := (gt_id n m).
 Notation "n <<  m" := (lt_id n m).
 
 Lemma lt_eq_lt_id_dec: forall (id1 id2 : id), {id1 << id2} + {id1 = id2} + {id2 << id1}.
-Proof. admit. Qed.
+Proof.
+  intros [n1]. induction n1 as [| n1' IH].
+  - intros [n2]. destruct n2 as [| n2'].
+    + auto.
+    + left. left. apply lt_conv. omega.
+  - intros [n2]. destruct n2 as [| n2'].
+    + right. apply lt_conv. omega.
+    + destruct (IH (Id n2')) as [[Hl | He] | Hg].
+      * left. left. inversion Hl. apply lt_conv. omega.
+      * left. right. inversion He. auto.
+      * right. inversion Hg. apply lt_conv. omega.
+Qed.
 
 Lemma gt_eq_gt_id_dec: forall (id1 id2 : id), {id1 >> id2} + {id1 = id2} + {id2 >> id1}.
-Proof. admit. Qed.
+Proof.
+  intros id1 id2. remember (lt_eq_lt_id_dec id1 id2) as L.
+  destruct L as [[Hl | He] | Hg].
+  - right. inversion Hl. apply gt_conv. omega.
+  - left. right. apply He.
+  - left. left. inversion Hg. apply gt_conv. omega.
+Qed.
 
 Lemma le_gt_id_dec : forall id1 id2 : id, {id1 <<= id2} + {id1 >> id2}.
-Proof. admit. Qed.
+Proof.
+  intros [n1] [n2]. remember (lt_eq_lt_id_dec (Id n1) (Id n2)) as L.
+  destruct L as [[Hl | He] | Hg].
+  - left. inversion Hl. apply le_conv. omega.
+  - left. inversion He. apply le_conv. omega.
+  - right. inversion Hg. apply gt_conv. omega.
+Qed.
 
 Lemma eq_dec : forall n m : nat, {n = m} + {n <> m}.
-Proof. admit. Qed.
+Proof.
+  induction n as [| n' IH].
+  - destruct m.
+    + left. reflexivity.
+    + right. omega.
+  - destruct m.
+    + right. omega.
+    + remember (IH m) as IHm. destruct IHm as [eqH | neqH].
+      * left. omega.
+      * right. omega.
+Qed.
 
 Lemma eq_id_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Qed. 
+Proof.
+  intros [n1] [n2]. destruct (eq_dec n1 n2) as [eqH | neqH].
+  - left. apply f_equal. apply eqH.
+  - right. intros eq. inversion eq as [eqH]. apply neqH. apply eqH.
+Qed.
 
-Lemma eq_id : forall (T:Type) x (p q:T), (if eq_id_dec x x then p else q) = p. 
-Proof. admit. Qed.
+Lemma eq_id : forall (T:Type) x (p q:T), (if eq_id_dec x x then p else q) = p.
+Proof.
+  intros T x p q. destruct (eq_id_dec x x) as [He | Hn].
+  - reflexivity.
+  - contradiction.
+Qed.
 
-Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if eq_id_dec x y then p else q) = q. 
-Proof. admit. Qed.
+Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if eq_id_dec x y then p else q) = q.
+Proof.
+  intros T x y p q H. destruct (eq_id_dec x y) as [He | Hn].
+  - contradiction.
+  - reflexivity.
+Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id, id1 >> id2 -> id2 >> id1 -> False.
-Proof. admit. Qed.
+Proof.
+  intros [n1] [n2]. intros H1. inversion H1. intros H3. inversion H3. omega.
+Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id, id2 <<= id1 -> id2 >> id1 -> False.
-Proof. admit. Qed.
+Proof.
+  intros [n1] [n2]. intros H1. inversion H1. intros H3. inversion H3. omega.
+Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, id1 <<= id2 -> {id1 = id2} + {id2 >> id1}.
-Proof. admit. Qed.
+Proof.
+  intros [n1] [n2] H. remember (gt_eq_gt_id_dec (Id n1) (Id n2)) as L.
+  destruct L as [[Hl | He] | Hg].
+  - assert (contra: False).
+    { apply le_gt_id_false with (id1 := Id n2) (id2 := Id n1).
+      apply H. apply Hl. }
+    inversion contra.
+  - auto.
+  - auto.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id, id1 <> id2 -> {id1 >> id2} + {id2 >> id1}.
-Proof. admit. Qed.
-    
-Lemma eq_gt_id_false : forall id1 id2 : id, id1 = id2 -> id1 >> id2 -> False.
-Proof. admit. Qed.
+Proof.
+  intros id1 id2 H. remember (gt_eq_gt_id_dec id1 id2) as L.
+  destruct L as [[Hl | He] | Hg].
+  - auto.
+  - remember (H He) as contra. inversion contra.
+  - auto.
+Qed.
 
+Lemma eq_gt_id_false : forall id1 id2 : id, id1 = id2 -> id1 >> id2 -> False.
+Proof.
+    intros [n1] [n2]. intros H1. inversion H1. intros H3. inversion H3. omega.
+Qed.
--- State.v
+++ State.v
@@ -10,7 +10,7 @@ Section S.
 
   Variable A : Set.
 
-  Definition state := list (id * A). 
+  Definition state := list (id * A).
 
   Reserved Notation "st / x => y" (at level 0).
 
@@ -25,28 +25,76 @@ Section S.
 
   Lemma state_deterministic: forall (st : state) (x : id) (n m : A),
     st / x => n -> st / x => m -> n = m.
-  Proof. admit. Qed.    
+  Proof.
+    intros st x n m. induction st.
+    - intros H. inversion H.
+    - intros Hn. inversion Hn.
+      + intros Hm. inversion Hm.
+        * reflexivity.
+        * contradiction.
+      + intros Hm. inversion Hm.
+        * symmetry in H5. contradiction.
+        * auto.
+  Qed.
 
   Lemma update_eq : forall (st : state) (x : id) (n : A),
     st [x <- n] / x => n.
-  Proof. admit. Qed.
+  Proof.
+    intros st x n. unfold update. apply st_binds_hd.
+  Qed.
 
   Lemma update_neq : forall (st : state) (x2 x1 : id) (n m : A),
     x2 <> x1 -> st / x1 => m -> st [x2 <- n] / x1 => m.
-  Proof. admit. Qed.
+  Proof.
+    intros st x2 x1 n m nH uH. unfold update. apply st_binds_tl.
+    - auto.
+    - auto.
+  Qed.
 
   Lemma update_shadow : forall (st : state) (x1 x2 : id) (n1 n2 m : A),
     st[x2 <- n1][x2 <- n2] / x1 => m -> st[x2 <- n2] / x1 => m.
-  Proof. admit. Qed.
+  Proof.
+    intros st x1 x2 n1 n2 m H. unfold update in H. inversion H.
+    - apply st_binds_hd.
+    - apply st_binds_tl.
+      + auto.
+      + inversion H6.
+        * symmetry in H7. contradiction.
+        * auto.
+  Qed.
 
   Lemma update_same : forall (st : state) (x1 x2 : id) (n1 m : A),
     st / x1 => n1 -> st / x2 => m -> st [x1 <- n1] / x2 => m.
-  Proof. admit. Qed.
+  Proof.
+    intros st x1 x2 n1 m nH mH. destruct (eq_id_dec x1 x2) as [eH | neH].
+    - rewrite eH. assert (mnH: m = n1).
+      { apply state_deterministic with (st := st) (x := x1).
+        rewrite eH. auto. auto. }
+      rewrite mnH. apply st_binds_hd.
+    - apply st_binds_tl. auto. auto.
+  Qed.
 
   Lemma update_permute : forall (st : state) (x1 x2 x3 : id) (n1 n2 m : A),
     x2 <> x1 -> 
     st [x2 <- n1][x1 <- n2] / x3 => m ->
     st [x1 <- n2][x2 <- n1] / x3 => m.
-  Proof. admit. Qed.  
+  Proof.
+    intros st x1 x2 x3 n1 n2 m neH H. destruct (eq_id_dec x1 x3) as [eH | nH].
+    - rewrite eH in H. apply st_binds_tl.
+      + rewrite <- eH. intros contra. symmetry in contra. contradiction.
+      + inversion H.
+        * rewrite eH. apply st_binds_hd.
+        * contradiction.
+    - inversion H.
+      + contradiction.
+      + destruct (eq_id_dec x3 x2) as [eH' | nH'].
+        * inversion H6.
+          { apply st_binds_hd. }
+          { contradiction. }
+        * apply st_binds_tl. auto. apply st_binds_tl. auto.
+          inversion H6.
+          { symmetry in H7. contradiction. }
+          { auto. }
+  Qed.
 
 End S.
